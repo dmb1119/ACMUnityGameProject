b7be4e6e978c7e5387930b72f87c96797e1c6475
Mega-Tom <thomas@Thomass-MacBook-Pro.local>
2018-03-10 09:53:06 -0700

    Added basic objects
    Added a player, walls, and a movement script.

---
+++ Robo Spy/Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public Rigidbody2D targ;
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		if(targ == null)
+			targ = this.GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			targ.AddForce(Vector2.down * speed);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			targ.AddForce(Vector2.up * speed);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			targ.AddForce(Vector2.right * speed);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			targ.AddForce(Vector2.left * speed);
+		}
+	}
+}
